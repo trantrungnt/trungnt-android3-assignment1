a1ca5d888cc5d865b7651512bfd2ef7fa70574fb
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-31 14:13:46 +0700

    UI RedActivity (temp)

---
+++ Weather/app/src/main/java/techkids/mad3/trungnt/weather/RedActivity.java
@@ -0,0 +1,15 @@
+package techkids.mad3.trungnt.weather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrungNT on 3/31/2016.
+ */
+public class RedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red);
+    }
+}
